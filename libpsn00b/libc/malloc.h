bf6411087d96c968ee8c37cf343e73592df21152
EngineersBox <toastcraft.info@gmail.com>
2024-02-08 00:49:38 +1100

    Fixed attributes for malloc/calloc/realloc/free

--- libpsn00b/libc/malloc.h
+++ libpsn00b/libc/malloc.h
@@ -10,28 +10,49 @@
 #include "gnu_version.h"
 #include "malloc_impl.h"
 
-__attribute__((weak)) void InitHeap(void* addr, size_t size);
-__attribute__((weak)) void TrackHeapUsage(ptrdiff_t alloc_incr);
-__attribute__((weak)) void GetHeapUsage(HeapUsage* usage);
+__attribute__((weak))
+void InitHeap(void* addr, size_t size);
 
-__attribute__((weak, hot)) void free(void* ptr);
+__attribute__((weak))
+void TrackHeapUsage(ptrdiff_t alloc_incr);
 
-__attribute__((weak, hot, malloc, alloc_size(1)
+__attribute__((weak))
+void GetHeapUsage(HeapUsage* usage);
+
+__attribute__((weak, hot))
+void free(void* ptr);
+
+__attribute__((
+	weak,
+	hot,
+	malloc,
+	alloc_size(1)
 #ifdef gnu_version_10
-, malloc(free, 1)
+	malloc(free, 1)
 #endif
-)) void* malloc(size_t size);
+))
+void* malloc(size_t size);
 
-__attribute__((weak, hot, malloc, alloc_size(1, 2)
+__attribute__((
+	weak,
+	hot,
+	malloc,
+	alloc_size(1, 2)
 #ifdef gnu_version_10
-, malloc(free, 1)
+	malloc(free, 1)
 #endif
-)) void* calloc(size_t num, size_t size);
+))
+void* calloc(size_t num, size_t size);
 
-__attribute__((weak, hot, alloc_size(2)
+__attribute__((
+	weak,
+	hot,
+	malloc,
+	alloc_size(2)
 #ifdef gnu_version_10
-, malloc(free, 2)
+	malloc(free, 1)
 #endif
-)) void* realloc(void* ptr, size_t size);
+))
+void* realloc(void* ptr, size_t size);
 
 #endif // _H_MALLOC_
